8d380219f61fa9388180658fd66cc270ef6b0a4d
Joaquín Antequera <joaquinantequera2002@gmail.com>
2022-11-18 19:55:54 +0100

    eq

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -14,10 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Fragmento1 fr = new Fragmento1();
         Fragmento2 fr2 = new Fragmento2();
+
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
+
         ft.add(R.layout.fragmento1, fr, "Fragmento1");
         ft.add(R.layout.fragmento2, fr2, "Fragmento2");
         ft.commit();
