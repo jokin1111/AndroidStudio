c91980727c43b291cd3e3d358e16343e4481a384
Joaquín Antequera <joaquinantequera2002@gmail.com>
2022-11-18 19:15:15 +0100

    eq

---
+++ app/src/main/java/com/example/fragment/Fragmento1.java
@@ -0,0 +1,22 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.fragment.R;
+
+public class Fragmento1 extends Fragment {
+
+    public Fragmento1(){
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        return inflater.inflate(R.layout.fragmento1, container, false);
+    }
+
+}
--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragmento1 fr = new Fragmento1();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.layout.fragmento1, fr, "Fragmento1");
+        ft.commit();
     }
+
 }
